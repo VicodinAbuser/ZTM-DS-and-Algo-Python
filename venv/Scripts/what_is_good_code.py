61e2f9d7dbc0e882a10bccddcea06afda452ac0e
Naman Agarwal <thenamanagarwal@gmail.com>
2020-06-14 00:39:03 +0530

    Second Commit - Added an array of length 100000 and looped over it 100000 times. This time there was a stark difference in the time taken to loop over the array. The time taken was 1.09 secs as compared to 0.0 seconds earlier.

--- venv/Scripts/what_is_good_code.py
+++ venv/Scripts/what_is_good_code.py
@@ -2,12 +2,14 @@ import time
 
 nemo = ['nemo']
 everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla']
+large = ['nemo' for i in range(100000)]
 def find_nemo(array):
     t0 = time.time()
-    for i in range(0,len(nemo)):
+    for i in range(0,len(array)):
         if array[i] == 'nemo':
             print("Found Nemo!!")
     t1 = time.time()
     print(f'The search took {t1-t0} seconds.')
 find_nemo(nemo)
 find_nemo(everyone)
+find_nemo(large)
