621f5ce90860cb91960ef2afd9484185907a70b2
Naman Agarwal <thenamanagarwal@gmail.com>
2020-06-16 14:51:10 +0530

    Added the smarter_reverse function

--- venv/Scripts/Data Structures/Arrays/Reversing_String.py
+++ venv/Scripts/Data Structures/Arrays/Reversing_String.py
@@ -6,12 +6,30 @@
 #one by one from the end to the beginning.
 
 def simple_reverse(string):
-    new_string = str()
+    new_string = []
     for i in range(len(string)-1, -1, -1): #The for loop runs from the last element to the first element of the original string
-        new_string = new_string + string[i] #The characters of the original string are added to the new string
-    return new_string
+        new_string.append(string[i]) #The characters of the original string are added to the new string
+    return ''.join(new_string) #The characters of the reversed array are joined to form a string
 
 string = "Hello"
 print(simple_reverse(string))
 #Since we only have to traverse the string once, the time complexity is O(n)
 #But since we are also creating a new array of the same size , the space complexity is also O(n)
+
+
+#A smarter way to do this , can be taking a pair of elements from either end of the string and swapping them
+#We have start at both the ends and continue swapping pairs till the middle of the string
+#This way we can avoid having to create a new array and save on space complexity while keeping time complexity at O(n)
+
+def swap(string, a, b): #Function which swaps two characters of a string
+    temp = string[a]
+    string[a] = string[b]
+    string[b] = temp
+    return string
+
+def smarter_reverse(string):
+    for i in range(len(string)//2):
+        string = swap(string, i, len(string)-i)
+    return string
+
+print(simple_reverse(string))
