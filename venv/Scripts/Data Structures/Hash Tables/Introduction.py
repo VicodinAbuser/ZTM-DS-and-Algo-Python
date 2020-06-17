b14dca0509706c28291be2aab796aa0b13db9b5c
Naman Agarwal <thenamanagarwal@gmail.com>
2020-06-17 23:29:41 +0530

    Corrected typo

--- venv/Scripts/Data Structures/Hash Tables/Introduction.py
+++ venv/Scripts/Data Structures/Hash Tables/Introduction.py
@@ -11,7 +11,7 @@
 #In general, the lookup, insert and delete operations all are very fast, in the order of O(1)
 #But in some cases, more than one keys can map to the same slot and that increases the time complexity by some margin,
 #although not by a lot in most cases. This is known as a collision.
-#Now, like for almost problem there is some sort of solution in the computer science world,
+#Now, like for almost all problem there is some sort of a solution in the computer science world,
 #collisions can also be resolved by numerous collison resolution techniques like open addressing and closed addressing
 
 #Enough details, let's look at how hash tables are implemented in Python using dictionaries.
