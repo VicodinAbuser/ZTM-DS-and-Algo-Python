0e5fd3b91acea5110ea1aa61ae81ecf509ecdd38
Naman Agarwal <thenamanagarwal@gmail.com>
2020-06-23 03:01:11 +0530

    Updated the remove method

--- venv/Scripts/Data Structures/Trees/Binary_Search_Tree.py
+++ venv/Scripts/Data Structures/Trees/Binary_Search_Tree.py
@@ -70,7 +70,83 @@ class BST():
                 elif current_node.data < data:
                     current_node = current_node.right
 
-'''
+
+#Finally comes the very complicated remove method.
+#This one is too complicated for me to explain while writing. So I'll just write the code down with some comments
+#explaining which conditions are being checked
+    def remove(self, data):
+        if self.root == None: #Tree is empty
+            return "Tree Is Empty"
+        current_node = self.root
+        parent_node = None
+        while current_node!=None: #Traversing the tree to reach the desired node or the end of the tree
+            if current_node.data > data:
+                parent_node = current_node
+                current_node = current_node.left
+            elif current_node.data < data:
+                parent_node = current_node
+                current_node = current_node.right
+            else: #Match is found. Different cases to be checked
+                #Node has left child only
+                if current_node.right == None:
+                    if parent_node == None:
+                        self.root = current_node.left
+                        return
+                    else:
+                        if parent_node.data > current_node.data:
+                            parent_node.left = current_node.left
+                            return
+                        else:
+                            parent_node.right = current_node.left
+                            return
+
+                #Node has right child only
+                elif current_node.left == None:
+                    if parent_node == None:
+                        self.root = current_node.right
+                        return
+                    else:
+                        if parent_node.data > current_node.data:
+                            parent_node.left = current_node.right
+                            return
+                        else:
+                            parent_node.right = current_node.left
+                            return
+
+                #Node has neither left nor right child
+                elif current_node.left == None and current_node.right == None:
+                    if parent_node == None: #Node to be deleted is root
+                        current_node = None
+                        return
+                    if parent_node.data > current_node.data:
+                        parent_node.left = None
+                        return
+                    else:
+                        parent_node.right = None
+                        return
+
+                #Node has both left and right child
+                elif current_node.left != None and current_node.right != None:
+                    del_node = current_node.right
+                    del_node_parent = current_node.right
+                    while del_node.left != None: #Loop to reach the leftmost node of the right subtree of the current node
+                        del_node_parent = del_node
+                        del_node = del_node.left
+                    current_node.data = del_node.data #The value to be replaced is copied
+                    if del_node == del_node_parent: #If the node to be deleted is the exact right child of the current node
+                        current_node.right = del_node.right
+                        return
+                    if del_node.right == None: #If the leftmost node of the right subtree of the current node has no right subtree
+                        del_node_parent.left = None
+                        return
+                    else: #If it has a right subtree, we simply link it to the parent of the del_node
+                        del_node_parent.left = del_node.right
+                        return
+        return "Not Found"
+
+
+
+
 my_bst = BST()
 my_bst.insert(5)
 my_bst.insert(3)
@@ -80,10 +156,68 @@ my_bst.insert(13)
 my_bst.insert(65)
 my_bst.insert(0)
 my_bst.insert(10)
-
-print(my_bst.root.data)
-print(my_bst.root.left.data)
-print(my_bst.root.right.data)
-print(my_bst.root.left.left.data)
-print(my_bst.search(65))
 '''
+            5
+        3       7
+    1               13
+0                10     65
+'''
+
+(my_bst.remove(13))
+'''
+            5
+        3       7
+    1               65
+0                10     
+'''
+my_bst.remove(5)
+'''
+            7
+        3       65
+    1        10     
+0                
+'''
+my_bst.remove(3)
+'''
+            7
+        1       65
+    0        10                     
+'''
+my_bst.remove(7)
+'''
+            10
+        1       65
+    0                
+'''
+my_bst.remove(1)
+'''
+            10
+        0       65
+                     
+'''
+my_bst.remove(0)
+'''
+            10
+                65
+                     
+'''
+my_bst.remove(10)
+'''
+           65
+                
+                     
+'''
+my_bst.remove(65)
+'''
+           
+                
+'''
+
+my_bst.insert(10)
+'''
+        10
+
+
+'''
+print(my_bst.root.data)
+#10
