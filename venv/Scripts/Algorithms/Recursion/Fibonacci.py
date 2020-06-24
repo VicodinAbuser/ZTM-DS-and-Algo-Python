e1268dd40be4fe87f70b3f1fc002c4d66933908c
Naman Agarwal <thenamanagarwal@gmail.com>
2020-06-25 00:42:00 +0530

    Added Fibonacci.py

---
+++ venv/Scripts/Algorithms/Recursion/Fibonacci.py
@@ -0,0 +1,26 @@
+#Given a number, we have to return the number at that index of the fibonacci sequence.
+#Fibonacci Sequence - 0 1 1 2 3 5 8 13 21 34 55 89 144 . . . .
+#For example, fibonacci(5) should return 5 as the 5th index (staring from 0) of the fibonacci sequence is the number 5
+#Again , we will do both the iterative and recursive solutions
+
+def iterative_fibonacci(index):
+    first_number = 0
+    second_number = 1
+    if index == 0:
+        return first_number
+    if index == 1:
+        return second_number
+    for i in range(2,index +1):
+        third_number = first_number + second_number
+        first_number = second_number
+        second_number = third_number
+    return third_number
+
+print(iterative_fibonacci(0))  #0
+print(iterative_fibonacci(1))  #1
+print(iterative_fibonacci(5))  #5
+print(iterative_fibonacci(7))  #13
+print(iterative_fibonacci(10)) #55
+print(iterative_fibonacci(12)) #144
+
+
